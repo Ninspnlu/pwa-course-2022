070360523b730a8fb628d9a0b84ab1dfbf314849
Ninspnlu <tanin.phom@bumail.net>
2022-02-25 21:48:46 +0700

    edit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-<<<<<<< HEAD
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-=======
-import reportWebVitals from './reportWebVitals';
-import Router from './Router.js'
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Router />
->>>>>>> parent of 03b8031 (fix)
   </React.StrictMode>,
   document.getElementById('root')
 );
