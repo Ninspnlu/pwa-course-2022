04b95c118ee1d51a52a2e82c6fe26745edfdf933
Ninspnlu <tanin.phom@bumail.net>
2022-03-19 10:30:52 +0700

    API commit

--- src/components/Chatline.js
+++ src/components/Chatline.js
@@ -26,9 +26,27 @@ function Chatline (props) {
         .then(data => setElement(data.gender));
     } 
     else if (text.startsWith("/randomfact")) {
+      fetch("https://uselessfacts.jsph.pl/random.json?language=en")
+        .then(res => res.json())
+        .then(data => setElement(data.text));
       // ex10-01 here: Implement randomfact using fetch & promise
       // https://uselessfacts.jsph.pl/random.json?language=en
-      
+    }
+    else if (text.startsWith("/cat")) {
+      fetch("https://catfact.ninja/fact")
+        .then(res => res.json())
+        .then(data => setElement("The facts about cats : " + data.fact));
+    }
+    else if (text.startsWith("/nationalize")) {
+      const [, name] = text.split(" ");
+      fetch("https://api.nationalize.io/?name="+name)
+        .then(res => res.json())
+        .then(data => setElement(data.name + " => " + data.country[0].country_id  ));
+    }
+    else if (text.startsWith("/bored")) {
+      fetch("https://www.boredapi.com/api/activity")
+        .then(res => res.json())
+        .then(data => setElement(data.activity + " About : " +data.type));
     }
   };
 
